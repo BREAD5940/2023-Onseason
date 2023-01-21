b30d5bd4988670123a5b1546d75f34161e43ceb6
rottingsquids <cleung26@dtechhs.org>
2023-01-21 14:13:51 -0800

    implemented end effector IO

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023-Beta";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2023-01-19 20:30:08 EST";
-  public static final long BUILD_UNIX_TIME = 1674178208235L;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "75466fb73756a1b604cbb1063bc74adbb1c5177b";
+  public static final String GIT_DATE = "2023-01-19 20:31:44 EST";
+  public static final String GIT_BRANCH = "end-effector";
+  public static final String BUILD_DATE = "2023-01-21 15:55:54 EST";
+  public static final long BUILD_UNIX_TIME = 1674334554515L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
