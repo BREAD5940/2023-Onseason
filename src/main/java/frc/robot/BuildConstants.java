6aa8579e5db1c37d30da82836c329ba2c6127d59
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2023-03-04 13:32:17 -0800

    bruh

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023-Beta";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "8262a2c90df509407760fc3d3db0ad78354e5066";
-  public static final String GIT_DATE = "2023-03-02 00:46:17 EST";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "c90c82c24894ff8c5d16f7b267304c681a0e5fed";
+  public static final String GIT_DATE = "2023-03-04 14:27:59 EST";
   public static final String GIT_BRANCH = "gamma";
-  public static final String BUILD_DATE = "2023-03-03 04:51:54 EST";
-  public static final long BUILD_UNIX_TIME = 1677837114699L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-03-04 16:29:52 EST";
+  public static final long BUILD_UNIX_TIME = 1677965392671L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
