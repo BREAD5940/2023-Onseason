c39ecb9297a35a4f8eb68f462132ef310929907b
Krish Kittur <73321517+KrishKittur@users.noreply.github.com>
2023-03-18 20:54:28 -0700

    More autos go brrr

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023-Beta";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 73;
-  public static final String GIT_SHA = "ed8df160b71d07aa77cb898714207b8971616c2b";
-  public static final String GIT_DATE = "2023-03-18 21:22:14 EDT";
+  public static final int GIT_REVISION = 74;
+  public static final String GIT_SHA = "1af0e89d17eb51ddbdc9afb819dc38342cff08d8";
+  public static final String GIT_DATE = "2023-03-18 23:19:41 EDT";
   public static final String GIT_BRANCH = "gamma";
-  public static final String BUILD_DATE = "2023-03-18 22:04:20 EDT";
-  public static final long BUILD_UNIX_TIME = 1679191460640L;
+  public static final String BUILD_DATE = "2023-03-18 23:47:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1679197639573L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
