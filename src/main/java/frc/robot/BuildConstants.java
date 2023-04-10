f87a528fd7487a61aa1ae68fd6e13c47b410714a
MackenzieChan <112980877+MackenzieChan@users.noreply.github.com>
2023-04-09 21:59:50 -0700

    change intake and swerve zeros

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023-Beta";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 106;
-  public static final String GIT_SHA = "122e16a8454b5724d19ef1ebebfdbfa9a435a506";
-  public static final String GIT_DATE = "2023-04-08 17:52:20 EDT";
-  public static final String GIT_BRANCH = "fix/auto-placement";
-  public static final String BUILD_DATE = "2023-04-08 18:14:54 EDT";
-  public static final long BUILD_UNIX_TIME = 1680992094206L;
+  public static final int GIT_REVISION = 112;
+  public static final String GIT_SHA = "064388e5f4539d5112f3eee6f8578f947e73420e";
+  public static final String GIT_DATE = "2023-04-08 18:59:43 EDT";
+  public static final String GIT_BRANCH = "gamma";
+  public static final String BUILD_DATE = "2023-04-10 00:47:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1681102039325L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
